a8e55556b785ccb44a129809070380fc5a8e8bf1
danny.mac <langzihankang@hotmail.com>
2022-11-18 14:47:08 +0800

    rm data.ts

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -81,6 +81,7 @@ Page({
     })
   },
   onLoad() {
+    wx.clearStorageSync()
     // @ts-ignore
     if (wx.getUserProfile) {
       this.setData({
@@ -132,13 +133,6 @@ Page({
   onInputLoseFocus(){
     this.setData({
       edit_class: "note",
-    })
-  },
-  getUserInfo(e: any) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   },
   getUserInput(e: any) {
